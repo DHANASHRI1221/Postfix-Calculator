a0cf55c92c640c6bcb15b35d61280dd199cc4f3c
Dhanashri Shivdas <152020940+DHANASHRI1221@users.noreply.github.com>
2024-01-02 21:40:39 +0530

    Adding Nothrow

--- list.cpp
+++ list.cpp
@@ -4,24 +4,8 @@
 
 using namespace std;
 
-//  List:: List(){
-//    size=0;
-   
-//  try{
-//  sentinel_head= new Node(true);
-//  sentinel_tail= new Node(true);
- 
- 
-//  sentinel_head->next=sentinel_tail;
-//  sentinel_tail->prev=sentinel_head;
-//  }
-//   catch (const bad_alloc& e){
-//         throw  std::runtime_error("Out Of Memory");
-//        }
-//  }
 List:: List(){
    size=0;
-//  try{
  sentinel_head= new(nothrow) Node(true);
  sentinel_tail= new(nothrow) Node(true);
  if(!sentinel_head || !sentinel_tail){
@@ -50,22 +34,6 @@ List:: List(){
 
  }
  
-//  void List::insert(int v){
-//    try{
-//    Node* n= new Node(v,nullptr,nullptr);
-//    n->prev=sentinel_tail->prev;
-//    n->next=sentinel_tail;
-//    sentinel_tail->prev->next=n;
-//    sentinel_tail->prev=n;
-//   size=size+1;
-//    }
-//    catch (const bad_alloc& e){
-//         throw  std::runtime_error("Out Of Memory");
-//        }
-   
-   
-  
-//  }
 void List::insert(int v) {
     // try {
         Node* n = new (nothrow) Node(v, nullptr, nullptr);
@@ -77,10 +45,6 @@ void List::insert(int v) {
         n->next = sentinel_tail;
         sentinel_tail->prev->next = n;
         sentinel_tail->prev = n;
-        size = size + 1;
-    // } catch (const bad_alloc& e) {
-    //     throw runtime_error("Memory allocation failed");
-    // }
 }
 Node* List:: get_head(){
   Node* pointer= sentinel_head;
